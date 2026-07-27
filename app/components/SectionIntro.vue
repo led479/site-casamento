66fb887e73cc06c21151ce68c2e4a2e6bf95f716
Rolf <rolf@fetch.ly>
2026-07-27 12:14:59 -0300

    Atualiza SectionIntro p/ não quebrar no pc

--- app/components/SectionIntro.vue
+++ app/components/SectionIntro.vue
@@ -2,12 +2,12 @@
   <section
     class="relative flex flex-col items-center justify-center w-full h-screen bg-cover px-8 py-14 bg-center text-intro-dark-gray-brown bg-intro-light-orange"
   >
-    <div class="flex flex-col items-center justify-center h-full w-full bg-contain bg-no-repeat bg-center px-20 text-center" :style="{ backgroundImage: `url(${IntroBg})` }">
+    <div class="flex flex-col items-center justify-center h-full w-full bg-contain bg-no-repeat bg-center px-20 text-center max-w-sm" :style="{ backgroundImage: `url(${IntroBg})` }">
       <div class="flex flex-col items-center gap-10 leading-tight text-sm">
         <img :src="IntroLogo" alt="Logo da Intro" class="w-20" >
-        <p class="">Estamos muito felizes em compartilhar este momento tão especial com você.</p>
-        <p class="">Depois de tantos momentos vividos juntos, chegou a hora de celebrar o início de um novo capítulo das nossas vidas.</p>
-        <p class="">Será uma alegria ter sua presença nesse dia tão importante.</p>
+        <p>Estamos muito felizes em compartilhar este momento tão especial com você.</p>
+        <p>Depois de tantos momentos vividos juntos, chegou a hora de celebrar o início de um novo capítulo das nossas vidas.</p>
+        <p>Será uma alegria ter sua presença nesse dia tão importante.</p>
       </div>
     </div>
   </section>
