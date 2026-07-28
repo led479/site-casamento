4d7dda7f32e678c35400a21b78aff8a38777a6c8
Rolf <rolf@fetch.ly>
2026-07-28 16:35:02 -0300

    Updates app color

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -36,9 +36,7 @@ export default defineNuxtConfig({
     head: {
       meta: [
         { name: 'viewport', content: 'width=device-width, initial-scale=1, viewport-fit=cover' },
-        { name: 'theme-color', content: '#7a8f96' },
-        { name: 'apple-mobile-web-app-capable', content: 'yes' },
-        { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
+        { name: 'theme-color', content: '#faf1e1' }
       ],
     },
   },
