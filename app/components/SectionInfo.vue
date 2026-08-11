335382a7359e21797587afcc8dd712da6302684e
Rolf <rolf@fetch.ly>
2026-08-11 16:32:23 -0300

    Implementa seção de informações

--- app/components/SectionInfo.vue
+++ app/components/SectionInfo.vue
@@ -1,17 +1,17 @@
 <template>
   <section
-    class="relative flex flex-col items-center justify-center w-full bg-cover bg-right px-8 py-14 text-informacoes-dark-gray-brown" :style="{ backgroundImage: `url(${InfoBg})` }"
+    class="relative flex flex-col items-center justify-center w-full bg-cover bg-right px-8 py-14 text-p5-dark-gray" :style="{ backgroundImage: `url(${Pg5Fundo})` }"
   >
-    <div class="flex flex-col items-center justify-center h-full w-full bg-contain bg-no-repeat bg-center pb-10 px-10 text-center max-w-sm min-h-[500px]" :style="{ backgroundImage: `url(${InfoSeda})` }">
+    <div class="flex flex-col items-center h-full w-full bg-contain bg-no-repeat bg-center pb-10 pt-22 text-center max-w-sm min-h-[500px]" :style="{ backgroundImage: `url(${Pg5Seda})` }">
       <div class="flex flex-col items-center gap-6 leading-tight text-sm">
-        <div class="flex flex-col gap-4 items-center">
-          <img :src="Flores" alt="Logo da Intro" class="w-10" >
-          <InfoTitulo class="w-48 h-7" />
+        <div class="flex flex-col gap-2 items-center">
+          <img :src="Pg5Icone" alt="Flores" class="w-10" >
+          <Pg5Titulo class="w-40 h-7" />
         </div>
-        <div class="flex flex-col gap-3 px-8 max-w-[260px]">
+        <div class="flex flex-col items-center gap-3 max-w-[200px] text-center">
           <p class="font-semibold text-nowrap">Alguns detalhes para o seu conforto:</p>
-          <p>Traje: Social / Esporte fino</p>
-          <p>Nossa celebração não foi preparada para receber crianças.<br> Agradecemos a compreensão e o carinho de todos.</p>
+          <p class="opacity-75">Traje: Social / Esporte fino</p>
+          <p class="max-w-[175px]">Nossa celebração não foi preparada para receber crianças.<br> Agradecemos a compreensão e o carinho de todos.</p>
         </div>
       </div>
     </div>
@@ -20,8 +20,8 @@
 
 
 <script setup lang="ts">
-import InfoSeda from '~/assets/images/info-seda.webp'
-import InfoBg from '~/assets/images/info-bg.webp'
-import InfoTitulo from '~/assets/info-titulo.svg'
-import Flores from '~/assets/images/flores.webp'
+import Pg5Seda from '~/assets/new/pg5_seda.webp'
+import Pg5Fundo from '~/assets/new/pg5_fundo.webp'
+import Pg5Titulo from '~/assets/new/pg5_titulo-cropped.svg'
+import Pg5Icone from '~/assets/new/pg5_icone.webp'
 </script>
