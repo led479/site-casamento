a7232396e38525944deab31e1f3212d85db3a565
Rolf <rolf@fetch.ly>
2026-08-11 15:58:16 -0300

    Reimplementa seção igreja

--- app/components/SectionIgreja.vue
+++ app/components/SectionIgreja.vue
@@ -1,18 +1,22 @@
 <template>
   <section
-    class="relative flex flex-col items-center justify-center w-full py-10 text-igreja-dark-gray bg-igreja-light-orange"
+    class="relative flex flex-col items-center justify-center w-full pt-10 pb-20 text-p3-dark-gray" :style="{ backgroundImage: `url(${Pg3Fundo})` }"
   >
+    <img :src="Pg3IconeCanto" alt="Canto Estilizado" width="48" class="absolute left-3 top-5 rotate-y-180" >
+    <img :src="Pg3IconeCanto" alt="Canto Estilizado" width="48" class="absolute right-3 top-5" >
+    <img :src="Pg3IconeCanto" alt="Canto Estilizado" width="48" class="absolute left-3 bottom-5 rotate-180" >
+    <img :src="Pg3IconeCanto" alt="Canto Estilizado" width="48" class="absolute right-3 bottom-5 rotate-180 rotate-y-180" >
     <div class="flex flex-col items-center gap-10">
-      <div class="flex flex-col items-center gap-4">
-        <IgrejaTitulo class="w-80 h-16" />
-        <img :src="IgrejaDesenho" alt="Desenho da Igreja" class="max-w-[400px] w-full" >
+      <div class="flex flex-col items-center gap-6">
+        <img :src="Pg3Titulo" alt="Cerimônia Religiosa" width="150"  >
+        <img :src="Pg3FotoIgreja" alt="Foto Igreja" width="320"  >
       </div>
-      <div class="flex flex-col items-center text-center px-10 gap-5 leading-tight">
-        <p class="font-heading font-semibold">Igreja Puríssimo Coração de Maria</p>
-        <p>Rua Arlíndo Pena, 85 - Santa Mônica, Florianópolis - SC, 88037-260</p>
-        <p class="font-heading font-semibold">24 de outubro de 2026, às 16h</p>
-        <p>Estacionamento no local</p>
-        <Button class="w-full bg-igreja-light-orange py-6 border-igreja-dark-gray mt-2" variant="outline" size="lg" as="a" href="https://maps.app.goo.gl/kt8KHErgMXE6MCUC9" target="_blank">VER NO MAPA</Button>
+      <div class="flex flex-col items-center text-center px-10 gap-7 opacity-75">
+        <p class="uppercase font-semibold">Igreja Puríssimo Coração de Maria</p>
+        <p class="font-extralight">Rua Arlíndo Pena, 85 - Santa Mônica, Florianópolis - SC, 88037-260</p>
+        <p class="font-semibold">24 de outubro de 2026, às 16h</p>
+        <p class="font-extralight">Estacionamento no local</p>
+        <Button class="w-full max-w-[200px] bg-transparent py-6 border-p3-dark-gray/20 border mt-8" variant="outline" size="lg" as="a" href="https://maps.app.goo.gl/kt8KHErgMXE6MCUC9" target="_blank">VER NO MAPA</Button>
       </div>
     </div>
   </section>
@@ -20,6 +24,10 @@
 
 
 <script setup lang="ts">
+import Pg3Fundo from '~/assets/new/pg3_fundo.webp'
+import Pg3Titulo from '~/assets/new/pg3_titulo.webp'
+import Pg3FotoIgreja from '~/assets/new/pg3_foto_igreja.webp'
+import Pg3IconeCanto from '~/assets/new/pg3_icone_canto.webp'
 import IgrejaTitulo from '~/assets/igreja-titulo.svg'
 import IgrejaDesenho from '~/assets/images/igreja-desenho.webp'
 import { Button } from '~/components/ui/button';
--- app/components/SectionIntro.vue
+++ app/components/SectionIntro.vue
@@ -2,10 +2,10 @@
   <section
     class="relative flex flex-col items-center justify-center w-full min-h-screen px-8 py-14 text-p2-dark-gray-brown bg-cover" :style="{ backgroundImage: `url(${Pg2Fundo})` }"
   >
-    <div class="flex flex-col items-center  px-20 text-center max-w-sm gap-14">
+    <div class="flex flex-col items-center px-20 text-center max-w-sm gap-14">
       <div class="flex flex-col items-center gap-10">
         <img :src="Pg2Selo" alt="Logo da Intro" class="w-26" >
-        <div class="flex flex-col items-center gap-8 leading-tight">
+        <div class="flex flex-col items-center gap-8 leading-tight opacity-75">
           <p>Estamos muito felizes em compartilhar este momento tão especial com você.</p>
           <p>Depois de tantos momentos vividos juntos, chegou a hora de celebrar o início de um novo capítulo das nossas vidas.</p>
           <p>Será uma alegria ter sua presença nesse dia tão importante.</p>
