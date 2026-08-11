b44a15bb92fad7f013465295ba5b9f91ba6c8ce6
Rolf <rolf@fetch.ly>
2026-08-11 15:00:34 -0300

    Finaliza pagina 1

--- app/components/SectionHero.vue
+++ app/components/SectionHero.vue
@@ -1,14 +1,14 @@
 <template>
   <section
     class="relative flex flex-col items-center justify-center w-full min-h-[calc(100svh-1rem)] bg-cover bg-center px-4 text-hero-light-orange"
-    :style="imageLoaded ? { backgroundImage: `url(${MainBg})` } : {}"
+    :style="imageLoaded ? { backgroundImage: `url(${Pg1Fundo})` } : {}"
   >
     <Transition name="fade">
       <AppLoader v-if="!imageLoaded" />
     </Transition>
 
-    <div class="flex flex-col">
-      <Logo class="w-80 h-20" />
+    <div class="flex items-center text-center flex-col px-4">
+      <img :src="Pg1TextoEstilizado" alt="Giulia e Rolf" width="330" >
       <h1>TE CONVIDAM PARA O SEU CASAMENTO</h1>
     </div>
     <p class="absolute bottom-10 font-heading text-xs">24 de outubro de 2026</p>
@@ -16,18 +16,18 @@
 </template>
 
 <script setup lang="ts">
-import Logo from '~/assets/logo.svg'
-import MainBg from '~/assets/images/main-bg.webp'
+import Pg1TextoEstilizado from '~/assets/new/pg1_texto_estilizado.webp'
+import Pg1Fundo from '~/assets/new/pg1_fundo.webp'
 
 useHead({
-  link: [{ rel: 'preload', as: 'image', href: MainBg }],
+  link: [{ rel: 'preload', as: 'image', href: Pg1Fundo }],
 })
 
 const imageLoaded = ref(false)
 
 onMounted(() => {
   const img = new Image()
-  img.src = MainBg
+  img.src = Pg1Fundo
   img.onload = () => {
     imageLoaded.value = true
   }
